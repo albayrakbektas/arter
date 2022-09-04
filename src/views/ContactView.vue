5cc10a515fc559d64ecc64060043a69c20b0cb1e
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-09-04 20:21:49 +0300

    create contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="contact"></div>
+  <LayoutPage>
+    <div class="page-grid">
+      <ContactInformation />
+      <GetInTouch />
+    </div>
+  </LayoutPage>
 </template>
 
 <script>
+import LayoutPage from "@/components/Layouts/LayoutPage";
+import ContactInformation from "@/components/Pages/Contact/ContactInformation";
+import GetInTouch from "@/components/Pages/Contact/GetInTouch";
 export default {
   name: "ContactView",
+  components: { GetInTouch, ContactInformation, LayoutPage },
 };
 </script>
 
