9c358e21167dd38eac2a27890abecb9682731d0e
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-09-08 02:26:09 +0300

    create portfolio details /single

---
+++ src/components/Cards/BgCard.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="row">
+    <div class="bg-img">
+      <div class="row-container">
+        <h1>
+          {{ card.h1Top }}
+          <br v-if="card.isBreakLine" />
+          {{ card.h1Bottom }}
+        </h1>
+        <p>{{ card.content }}</p>
+        <slot></slot>
+        <MainButton :button="button" />
+      </div>
+      <img
+        v-if="card.isImg"
+        class="profile-photo"
+        src="../../assets/img/face-2.png"
+        alt="profile photo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import MainButton from "@/components/Pages/Publication/MainButton";
+export default {
+  name: "BgCard",
+  components: { MainButton },
+  props: {
+    card: Object,
+    button: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 46px;
+  font-family: Poppins, sans-serif;
+  margin: 0;
+}
+.bg-img {
+  background-image: url("../../assets/img/home/bg.jpeg");
+  left: 30px;
+  right: 30px;
+  height: 420px;
+}
+.row-container {
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+  height: calc(100% - 120px);
+  padding: 60px 30px;
+  text-align: center;
+  background-image: -webkit-gradient(
+    linear,
+    left top,
+    right top,
+    color-stop(15%, rgba(45, 45, 58, 0.9)),
+    color-stop(50%, rgba(45, 45, 58, 0.7)),
+    to(rgba(43, 43, 53, 0.7))
+  );
+}
+.router-button {
+  background-color: #ffc107;
+  padding: 5px 35px;
+  color: #20202a;
+  font-size: 11px;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  transition: 0.4s ease-in-out;
+  &:hover {
+    transform: translateY(-2px);
+  }
+}
+.row {
+  height: 420px;
+}
+p {
+  font-size: 16px;
+  color: #fafafc;
+  font-family: "Courier Prime", monospace;
+}
+@media (min-width: 900px) {
+  .row {
+    position: relative;
+    margin: 60px 0 0;
+  }
+  .bg-img {
+    left: 15px;
+    right: 15px;
+    bottom: 0;
+    height: unset;
+  }
+  .row-container {
+    justify-content: center;
+    justify-items: center;
+    text-align: center;
+    padding: 60px;
+  }
+  .router-button {
+    justify-content: start;
+    margin: unset;
+    width: fit-content;
+  }
+  img {
+    position: absolute;
+    height: 100%;
+    width: 360px;
+    right: 30px;
+    top: 0;
+    bottom: 0;
+  }
+}
+</style>
---
+++ src/views/SingleProject.vue
@@ -0,0 +1,101 @@
+<template>
+  <LayoutPage>
+    <PublicationHeader :card="header" class="pd header-width" />
+    <RecommendationCards v-if="$store.state.layoutGrid === 'single-project'" />
+    <PublicationFullImg
+      v-if="$store.state.layoutGrid === 'single-project-two'"
+      class="full-img"
+      :img="img"
+    />
+    <ProjectDetails />
+    <PortfolioResult v-if="$store.state.layoutGrid === 'single-project-two'" />
+    <RecommendationCards :header="header2" />
+    <ExperienceCards class="mt" />
+    <BgCard :button="button" :card="card" class="rel-row mt" />
+    <FooterNavigation :card="footerCard" class="pd" />
+  </LayoutPage>
+</template>
+
+<script>
+import LayoutPage from "@/components/Layouts/LayoutPage";
+import ProjectDetails from "@/components/Pages/SingleProject/ProjectDetails";
+import BgCard from "@/components/Cards/BgCard";
+import FooterNavigation from "@/components/Pages/Publication/FooterNavigation";
+import ExperienceCards from "@/components/Pages/Home/experience/ExperienceCards";
+import RecommendationCards from "@/components/Pages/Home/recommendation/RecommendationCards";
+import PublicationFullImg from "@/components/Pages/Publication/PublicationFullImg";
+import PublicationHeader from "@/components/Pages/Publication/PublicationHeader";
+import PortfolioResult from "@/components/Pages/SingleProject/PortfolioResult";
+export default {
+  name: "SingleProject",
+  components: {
+    PortfolioResult,
+    PublicationHeader,
+    PublicationFullImg,
+    RecommendationCards,
+    ExperienceCards,
+    FooterNavigation,
+    BgCard,
+    ProjectDetails,
+    LayoutPage,
+  },
+  data() {
+    return {
+      header: {
+        title: "Title",
+        content: "Ui Design, Graphic",
+      },
+      header2: {
+        title: "Client reviews",
+      },
+      img: {
+        src: require("../assets/img/blog/publication/2.jpeg"),
+        alt: "",
+      },
+      button: {
+        content: "CONTACT ME",
+        href: "contact",
+      },
+      card: {
+        h1Top: "Ready to order your project?",
+        content: "Let's work together!",
+        isImg: false,
+      },
+      footerCard: {
+        prevLink: "previous project",
+        allLink: {
+          content: "all projects",
+          href: "#",
+          route: "portfolio",
+        },
+        nextLink: "next project",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.rel-row {
+  position: relative !important;
+  h1 {
+    font-weight: 800;
+  }
+}
+.pd {
+  margin-left: 15px;
+  margin-right: 15px;
+}
+.full-img {
+  left: 15px;
+  right: 15px;
+}
+.header-width {
+  width: calc(100% - 30px);
+}
+@media (min-width: 900px) {
+  .mt {
+    margin-top: 0 !important;
+  }
+}
+</style>
