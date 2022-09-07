9d079709b7ccbd492b718502b16dfcca1765f0aa
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-09-07 21:58:53 +0300

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>portfolio</div>
+  <LayoutPage>
+    <PortfolioRouters />
+    <PortfolioTwoColumn />
+  </LayoutPage>
 </template>
 
 <script>
+import LayoutPage from "@/components/Layouts/LayoutPage";
+import PortfolioTwoColumn from "@/components/Pages/Portfolio/PortfolioTwoColumn";
+import PortfolioRouters from "@/components/Pages/Portfolio/PortfolioRouters";
 export default {
   name: "PortfolioView",
+  components: { PortfolioRouters, PortfolioTwoColumn, LayoutPage },
 };
 </script>
 
