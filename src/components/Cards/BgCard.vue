00dd791264f3e071cca781f5b50be39872e10a32
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-09-08 15:30:49 +0300

    Update BgCard.vue

--- src/components/Cards/BgCard.vue
+++ src/components/Cards/BgCard.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="row">
-    <div class="bg-img">
-      <div class="row-container">
-        <h1>
-          {{ card.h1Top }}
-          <br v-if="card.isBreakLine" />
-          {{ card.h1Bottom }}
-        </h1>
-        <p>{{ card.content }}</p>
-        <slot></slot>
-        <MainButton :button="button" />
+  <div ref="row" class="row">
+    <div ref="bg-img" class="bg-img">
+      <div class="overlay">
+        <div class="row-container">
+          <h1>
+            {{ card.h1Top }}
+            <br v-if="card.isBreakLine" />
+            {{ card.h1Bottom }}
+          </h1>
+          <p>{{ card.content }}</p>
+          <slot></slot>
+          <MainButton :button="button" />
+        </div>
       </div>
       <img
         v-if="card.isImg"
@@ -30,6 +32,20 @@ export default {
     card: Object,
     button: Object,
   },
+  data() {
+    return {
+      myHeight: 0,
+    };
+  },
+  mounted() {
+    this.setHeight();
+  },
+  methods: {
+    setHeight() {
+      this.$refs.row.style.height =
+        Number(this.$refs["bg-img"].offsetHeight) + "px";
+    },
+  },
 };
 </script>
 
@@ -43,39 +59,41 @@ h1 {
   background-image: url("../../assets/img/home/bg.jpeg");
   left: 30px;
   right: 30px;
-  height: 420px;
+}
+.overlay {
+  position: relative;
+  display: flex;
+  padding: 60px;
+  height: 100%;
+  width: calc(100% - 120px);
+  background-image: linear-gradient(
+    90deg,
+    rgba(45, 45, 58, 0.9) 15%,
+    rgba(45, 45, 58, 0.7) 50%,
+    rgba(43, 43, 53, 0.7) 100%
+  );
 }
 .row-container {
-  display: grid;
-  grid-template-rows: repeat(3, auto);
-  height: calc(100% - 120px);
-  padding: 60px 30px;
+  display: block;
   text-align: center;
-  background-image: -webkit-gradient(
-    linear,
-    left top,
-    right top,
-    color-stop(15%, rgba(45, 45, 58, 0.9)),
-    color-stop(50%, rgba(45, 45, 58, 0.7)),
-    to(rgba(43, 43, 53, 0.7))
-  );
+  width: 100%;
 }
 .router-button {
   background-color: #ffc107;
-  padding: 5px 35px;
+  height: 45px;
+  padding: 0 35px;
+  font-weight: 600;
   color: #20202a;
   font-size: 11px;
   display: grid;
   align-items: center;
   justify-content: center;
   margin: 0 auto;
+  width: max-content;
   transition: 0.4s ease-in-out;
   &:hover {
     transform: translateY(-2px);
   }
-}
-.row {
-  height: 420px;
 }
 p {
   font-size: 16px;
@@ -85,24 +103,16 @@ p {
 @media (min-width: 900px) {
   .row {
     position: relative;
-    margin: 60px 0 0;
   }
   .bg-img {
-    left: 15px;
-    right: 15px;
-    bottom: 0;
-    height: unset;
   }
   .row-container {
     justify-content: center;
     justify-items: center;
-    text-align: center;
-    padding: 60px;
+    align-self: center;
   }
   .router-button {
     justify-content: start;
-    margin: unset;
-    width: fit-content;
   }
   img {
     position: absolute;
