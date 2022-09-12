3f4db643e07f5c36c294817c46e02cb79df1ac9a
Bektas Albayrak <albayrakbektas44@gmail.com>
2022-09-12 21:07:26 +0300

    Create BekoPagination.vue

---
+++ src/components/Carousel/BekoPagination.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="beko-pagination">
+    <div class="beko-carousel-dot-container">
+      <button
+        v-for="(item, index) in paginationCount"
+        :key="`${item}_${index}`"
+        class="beko-carousel-dot"
+        :class="{ 'beko-carousel-dot--active': isCurrentDot(index) }"
+        v-on:click="goToPage(index)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BekoPagination",
+  inject: ["carousel"],
+  computed: {
+    paginationCount() {
+      return this.carousel && this.carousel.scrollPerPage
+        ? this.carousel.pageCount
+        : this.carousel.slideCount || 0;
+    },
+  },
+  methods: {
+    isCurrentDot(index) {
+      return index === this.carousel.currentPage;
+    },
+    goToPage(index) {
+      this.$emit("paginationclick", index);
+      this.carousel.activeSlides();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.beko-pagination {
+  position: absolute;
+  left: 0;
+  bottom: -30px;
+}
+.beko-carousel-dot {
+  width: 8px;
+  display: inline-block;
+  margin-right: 10px;
+  background-color: #8c8c8e;
+  border-radius: 5px;
+  opacity: 0.5;
+  height: 4px;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+}
+.beko-carousel-dot--active {
+  width: 20px;
+  background-color: #ffc107;
+  opacity: 1;
+}
+button {
+  cursor: pointer;
+  outline: none;
+  border: none;
+  background: transparent;
+}
+</style>
